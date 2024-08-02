b5a1b12a7deb64e7eeaa22842e645660f81c6891
samuelmartins22 <samuelcruzeiro72@gmail.com>
2024-08-02 10:54:12 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { Component } from 'react'; 
 import './App.css';
+import { Posts } from './components/posts/index.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { loadPosts } from './utils/load-posts'
+
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  async componentDidMount() {
+  await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+  
+  const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  }
+  
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
 }
 
 export default App;
+
+
+//import { Component } from 'react';
+
+//import './App.css';
+//import {PostCard} from './components/PostCard';
+
+//import { loadPosts } from './utils/load-posts';
+
+//class App extends Component {
+  //state = {
+    //posts: []
+  //};
+
+  //async componentDidMount() {
+    //await this.loadPosts();
+  //}
+  
+    //loadPosts= async () => {
+    //const postsAndPhotos = await loadPosts();
+    //this.setState ({ posts: postsAndPhotos });
+    //}
+
+  //render() {
+    //const { posts } = this.state;
+
+
+    // componente
+    //return (
+      //<section className="container">
+        //<posts posts={posts} />
+      //</section>
+    //);
+ //}
+//}
+
+
